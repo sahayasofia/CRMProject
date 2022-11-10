d1025c9abd2b186b1c388896433169f1c0f3b1ca
sahayasofia <sahaysofia2002@gmail.com>
2022-11-10 15:36:05 +0530

    project1

---
+++ src/com/faith/testDb/TestDbServlet.java
@@ -0,0 +1,49 @@
+package com.faith.testDb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//SERVLET implementation class
+@WebServlet("/testdbservlet")
+public class TestDbServlet extends HttpServlet {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// setup Connection variables
+		String user="root";
+		String password="sofia@31";
+		String jdbcurl="jdbc:mysql://localhost:3306/democrm2022?serverTimezone=UTC";
+		String driverclass="com.mysql.cj.jdbc.Driver";
+		
+		//get connection 
+		try {
+			PrintWriter out=resp.getWriter();
+			out.println("Connecting to database:"+jdbcurl+"/n");
+			//set connection String
+			Class.forName(driverclass);
+			Connection connection=DriverManager.getConnection(jdbcurl,user,password);
+			out.println("SUCCESS!!!!!");
+			//close connection
+			connection.close();
+			
+			
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			throw new ServletException();
+		}
+	}
+	
+	
+
+}
